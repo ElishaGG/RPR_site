8de94f5e7cd4549904cf234e905e66ff046173a8
Elisey Naumov <elisey.user@gmail.com>
2024-06-16 16:43:16 +0300

    fix a mail.php. add validatoin for empty inputs from form.

--- mail.php
+++ mail.php
@@ -1,50 +1,59 @@
 <?php
-    $name = $_POST['name'];
-    $telephone = $_POST['telephone'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
 
-    $to = "pavel.h@rpr.org.ru";
-    $subject = "Запрос с сайта";
-    $headers = "From: naumov.e@rpr.org.ru\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers .= "Cc: naumov.e@rpr.org.ru\r\n";
-    
-    $message = "
-        <html>
-            <body>
-                <div>
-                    <style>
-                        body, html {padding: 0;margin: 0;width: 100%;height: 100%;box-sizing: border-box;}
-                        div {background-color: #eeeeee;margin: 0px;padding: 10px;width: 100%;height: 100%;box-sizing: border-box;}
-                        h2,p {margin: 0;padding: 10px 0;box-sizing: border-box;}
-                        a,span {margin: 0px 5px;}
-                    </style>
-                    <h2>Запрос с сайта</h2>
-                    <hr>
-                    <p><strong>Имя: </strong><span style='letter-spacing:2px; text-decoration:none;color:#333;'>$name</span></p>
-                    <p><strong>Телефон: </strong>
-                        <a href='tel:$telephone'
-                        style='letter-spacing:2px; text-decoration:none;color:#333;border-bottom: 1px #333 dotted;'>$telephone
-                        </a>
-                    </p>
-                    <p>
-                        <strong>Email: </strong>
-                        <a href='mailto:$email'
-                        style='letter-spacing:2px; text-decoration:none;color:#333;border-bottom: 1px #333 dotted;'>$email</a>
-                    </p>
-                    <hr>
-                    <p><strong>Сообщение: </strong><span style='color:#333;'><span style='letter-spacing:2px; text-decoration:none;color:#333;'>$message</span></span></p>
-                </div>
-            </body>
-        </html>
-    ";
+$name = $_POST['name'];
+$telephone = $_POST['telephone'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-    if(mail($to, $subject, $message, $headers)) {
-        echo "Письмо успешно отправлено";
-    } else {
-        http_response_code(500);
-        echo "Ошибка при отправке письма";
-    }
+$ip_address = $_SERVER['REMOTE_ADDR'];
 
+if (empty($name) || empty($telephone) || empty($email) || empty($message)) {
+    http_response_code(400);
+    exit();
+}
+
+$to = "pavel.h@rpr.org.ru";
+$subject = "Запрос с сайта";
+$headers = "From: naumov.e@rpr.org.ru\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+$headers .= "Cc: naumov.e@rpr.org.ru\r\n";
+
+$message = "
+    <html>
+        <body>
+            <div>
+                <style>
+                    body, html {padding: 0;margin: 0;width: 100%;height: 100%;box-sizing: border-box;}
+                    div {background-color: #eeeeee;margin: 0px;padding: 10px;width: 100%;height: 100%;box-sizing: border-box;}
+                    h2,p {margin: 0;padding: 10px 0;box-sizing: border-box;}
+                    a,span {margin: 0px 5px;}
+                </style>
+                <h2>Запрос с сайта</h2>
+                <hr>
+                <p><strong>Имя: </strong><span style='letter-spacing:2px; text-decoration:none;color:#333;'>$name</span></p>
+                <p><strong>Телефон: </strong>
+                    <a href='tel:$telephone'
+                    style='letter-spacing:2px; text-decoration:none;color:#333;border-bottom: 1px #333 dotted;'>$telephone
+                    </a>
+                </p>
+                <p>
+                    <strong>Email: </strong>
+                    <a href='mailto:$email'
+                    style='letter-spacing:2px; text-decoration:none;color:#333;border-bottom: 1px #333 dotted;'>$email</a>
+                </p>
+                <hr>
+                <p><strong>Сообщение: </strong><span style='color:#333;'><span style='letter-spacing:2px; text-decoration:none;color:#333;'>$message</span></span></p>
+                <hr>
+                <p><strong>IP-адрес отправителя: </strong><span style='letter-spacing:2px; text-decoration:none;color:#333;'>$ip_address</span></p>
+            </div>
+        </body>
+    </html>
+";
+
+if(mail($to, $subject, $message, $headers)) {
+    echo "Письмо успешно отправлено";
+} else {
+    http_response_code(500);
+    echo "Ошибка при отправке письма";
+}
 ?>
